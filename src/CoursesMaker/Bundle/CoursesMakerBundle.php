83d560219af425f9cdac1d60d3bbb9c27a3f1ae6
Laurent BILLON <laurent.billonc@gmail.com>
2013-01-22 17:44:42 +0100

    Added coursesBundle

---
+++ src/CoursesMaker/Bundle/CoursesMakerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CoursesMaker\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CoursesMakerBundle extends Bundle
+{
+}
---
+++ src/CoursesMaker/Bundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CoursesMaker\Bundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello")')->count() > 0);
+    }
+}
