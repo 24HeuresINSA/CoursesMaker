65b0a46e6751a1a0fafe645d5c954ee883cc9649
Romaric Drigon <romaric.drigon@gmail.com>
2013-03-30 18:47:04 +0100

    Better security tests

--- src/Rotis/CourseMakerBundle/Tests/Controller/SecurityController.php
+++ src/Rotis/CourseMakerBundle/Tests/Controller/SecurityController.php
@@ -29,30 +29,58 @@ class SecurityController extends ControllerTest
         ;
     }
 
+    /*
+     * This tests needs a user "admin" / "admin"
+     */
     public function testSuccessfullLogin()
     {
         $this
-            ->request(array('username' => 'admin', 'password' => 'admin')) // of course data-dependent!
-                ->POST('/login')
-                ->hasStatus(200)
-                ->hasHeader('Content-Type', 'text/html; charset=UTF-8')
+            ->request()
+                ->GET('/login') // Symfony2 prevent login forms to be send from other pages
+                    ->hasStatus(200)
+                    ->hasHeader('Content-Type', 'text/html; charset=UTF-8')
+                ->POST('/login_check', array('_username' => 'admin', '_password' => 'admin', '_target_path' => '/'))
+                    ->hasStatus(302) // redirection
+                ->GET('/') // go to accueil
+                    ->hasStatus(200)
+                    ->crawler
+                        ->hasElement('.hero-unit') # main title, "hero" block
+                        ->end()
+                        ->hasElement('a')
+                            ->withContent('admin')
+                        ->end()
+                        ->hasElement('a')
+                            ->withContent('Infos Pratiques')
+                        ->end()
+                        ->hasElement('a')
+                            ->withContent('Déconnexion')
+                        ->end()
         ;
     }
 
     public function testWrongLogin()
     {
         $this
-            ->request(array('username' => 'toto', 'password' => 'ghjkjhvghj'))
-                ->POST('/login')
-                ->hasStatus(200)
-                ->hasHeader('Content-Type', 'text/html; charset=UTF-8')
-                ->crawler // back to the login page
-                    ->hasElement('#username')
-                        //->withAttribute('value', 'toto')
-                    ->end()
-                    ->hasElement('#password')
-                        ->hasNoContent()
-                    ->end()
+            ->request()
+                ->GET('/login') // Symfony2 prevent login forms to be send from other pages
+                    ->hasStatus(200)
+                    ->hasHeader('Content-Type', 'text/html; charset=UTF-8')
+                ->POST('/login_check', array('_username' => 'toto', '_password' => 'fghjkhgfdghjk', '_target_path' => '/'))
+                    ->hasStatus(302) // redirection
+                    ->crawler
+                        ->hasElement('a')
+                            ->withContent('http://localhost/login') // we are being redirected to login page
+                        ->end()
+                ->GET('/') // go to accueil
+                    ->hasStatus(200)
+                    ->crawler
+                        ->hasElement('a')
+                            ->withContent('Déconnexion')
+                            ->exactly(0) // not!
+                        ->end()
+                        ->hasElement('a')
+                            ->withContent('Connexion')
+                        ->end()
         ;
     }
 
@@ -69,23 +97,24 @@ class SecurityController extends ControllerTest
         ;
     }
 
-    public function testAccessDeniedEquipe()
+    public function testAccessDenied()
     {
         $this
             ->request()
                 ->GET('/admin/equipe')
                     ->hasStatus(302)
                     ->hasHeader('Content-Type', 'text/html; charset=UTF-8')
-        ;
-    }
-
-    public function testAccessDeniedCourse()
-    {
-        $this
-            ->request()
+                    ->crawler
+                        ->hasElement('a')
+                            ->withContent('http://localhost/login') // we are being redirected to login page
+                        ->end()
                 ->GET('/admin/course')
                     ->hasStatus(302)
                     ->hasHeader('Content-Type', 'text/html; charset=UTF-8')
+                    ->crawler
+                        ->hasElement('a')
+                            ->withContent('http://localhost/login') // we are being redirected to login page
+                        ->end()
         ;
     }
 }
