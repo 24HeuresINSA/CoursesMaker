393775100fccccb6baae422df02aa62023b1c5cf
Romaric Drigon <romaric.drigon@gmail.com>
2013-05-10 15:39:57 +0200

    Completed JoueurAdmin

--- src/Rotis/CourseMakerBundle/Admin/JoueurAdmin.php
+++ src/Rotis/CourseMakerBundle/Admin/JoueurAdmin.php
@@ -11,33 +11,95 @@ class JoueurAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('telephone')
-            ->add('etudiant', null, array('required' => false))
-            ->add('taille_tshirt')
-            ->add('papiers_ok', null, array('required' => false))
-            ->add('paiement_ok', null, array('required' => false));
+            ->add('nom', null, array(
+                    'label' => 'nom'
+                ))
+            ->add('prenom', null, array(
+                    'label' => 'Prénom'
+                ))
+            ->add('email', null, array(
+                    'label' => 'Adresse e-mail'
+                ))
+            ->add('telephone', null, array(
+                    'label' => 'Numéro de téléphone'
+                ))
+            ->add('etudiant', null, array(
+                    'required' => false,
+                    'label' => 'Étudiant'
+                ))
+            ->add('taille_tshirt', null, array(
+                    'required' => false,
+                    'label' => 'Taille T-shirt'
+                ))
+            ->add('papiers_ok', null, array(
+                    'required' => false,
+                    'label' => 'Papiers OK'
+                ))
+            ->add('paiement_ok', null, array(
+                    'required' => false,
+                    'label' => 'Paiement OK'
+                ))
+            ->add('equipe', null, array(
+                    'label' => 'Équipe'
+                ))
+        ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('nom')
-            ->add('etudiant')
-            ->add('papiers_ok')
-            ->add('paiement_ok');
+            ->add('nom', null, array(
+                    'label' => 'nom'
+                ))
+            ->add('prenom', null, array(
+                    'label' => 'Prénom'
+                ))
+            ->add('etudiant', null, array(
+                    'required' => false,
+                    'label' => 'Étudiant'
+                ))
+            ->add('taille_tshirt', null, array(
+                    'required' => false,
+                    'label' => 'Taille T-shirt'
+                ))
+            ->add('papiers_ok', null, array(
+                    'required' => false,
+                    'label' => 'Papiers OK'
+                ))
+            ->add('paiement_ok', null, array(
+                    'required' => false,
+                    'label' => 'Paiement OK'
+                ))
+            ->add('equipe', null, array(
+                    'label' => 'Équipe'
+                ))
+            ->add('equipe.course', null, array(
+                    'label' => 'Course'
+                ))
+            ->add('equipe.categorie', null, array(
+                    'label' => 'Catégorie'
+                ))
+        ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('id')
-            ->add('nom')
-            ->add('prenom')
-            ->add('etudiant')
-            ->add('papiers_ok')
-            ->add('paiement_ok');
+            ->addIdentifier('nom', null, array(
+                    'label' => 'nom'
+                ))
+            ->addIdentifier('prenom', null, array(
+                    'label' => 'Prénom'
+                ))
+            ->add('equipe', null, array(
+                    'label' => 'Équipe'
+                ))
+            ->add('equipe.course', null, array(
+                    'label' => 'Course'
+                ))
+            ->add('equipe.categorie', null, array(
+                    'label' => 'Catégorie'
+                ))
+        ;
     }
 }
